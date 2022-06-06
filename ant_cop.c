9e15ee17484766448b84e1f8813202295987f46d
wsdonnelly <wsdonnelly@gmail.com>
2022-06-06 21:06:31 +0300

    home work monday night

--- ant_cop.c
+++ ant_cop.c
@@ -1,14 +1,34 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ant_cop.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/05 14:10:25 by wdonnell          #+#    #+#             */
-/*   Updated: 2022/06/06 13:36:47 by wdonnell         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+**ANT_COP 1.0
+*/
+        _.---._
+     .-' ((O)) '-.
+      \ _.\_/._ /
+       /..___..\
+       ;-.___.-;
+      (| e ) e |)     .;.
+       \  /_   /      ||||
+       _\__-__/_    (\|'-|
+     /` / \V/ \ `\   \ )/
+    /   \  Y  /   \  /=/
+   /  |  \ | / {}  \/ /
+  /  /|   `|'   |\   /
+  \  \|    |.   | \_/
+   \ /\    |.   |
+    \_/\   |.   |
+    /)_/   |    |
+   // ',__.'.__,'
+  //   |   |   |
+ //    |   |   |
+(/     |   |   |
+       |   |   |
+       | _ | _ |
+       |   |   |
+       |   |   |
+       |   |   |
+       |___|___|
+       /  J L  \
+      (__/   \__)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,26 +37,26 @@
 char	**ft_strsplit(char const *s, char c);
 void	free_str_arr(char **arr);
 
-void add_to_arr(char *line, char **arr, int *ants_arr, int num_ants)
+void add_to_arr(char *line, char **arr, int *ants_arr, int num_ants, char *start, char *end)
 {
 	char **ln;
 	char **info;
 	int i = 0;
 	int j;
+	char *temp;
 
+	j = 0;
+	while (j < num_ants)
+	{
+		ants_arr[j] = 0;
+		j++;
+	}
 	ln = ft_strsplit(line, ' ');
 	while (ln[i])
 	{
-		j = 0;
-		while (j < num_ants)
-		{
-			ants_arr[j] = 0;
-			j++;
-		}
 		info = ft_strsplit(ln[i], '-');
 		if (ants_arr[atoi(&info[0][1]) - 1])
 		{
-			//same ant on same line
 			printf("same ant on same line\n");
 			exit(0);
 		}
@@ -50,19 +70,22 @@ void add_to_arr(char *line, char **arr, int *ants_arr, int num_ants)
 		j = 0;
 		while (j < num_ants)
 		{
-			if (info[1][0] == arr[j][0] && strcmp(info[1], ))
+			if (strcmp(info[1], start) && strcmp(info[1], end))
 			{
-				if (!strcmp(info[1], arr[j]))
+				if (info[1][0] == arr[j][0])
 				{
-					//same room on same line
-					printf("same room on same line\n");
-					exit(0);
+					if (!strcmp(info[1], arr[j]))
+					{
+						printf("same room on same line\n");
+						exit(0);
+					}
 				}
 			}
 			j++;
 		}
+		temp = arr[atoi(&info[0][1]) - 1];
 		arr[atoi(&info[0][1]) - 1] = strdup(info[1]);
-
+		free(temp);
 		free_str_arr(info);
 		i++;
 		
@@ -81,12 +104,6 @@ int valid_path(char *rm1, char *rm2)
 {
 
 }
-
-int ants_in_same_room(char *line, char **arr)
-{
-
-	return (1);
-}
 */
 int	main()
 {
@@ -100,6 +117,7 @@ int	main()
 	char *p;
 	char *start;
 	char *end;
+	char **start_end;
 
 	getline(&line, &n, stdin);
 	num_ants = atoi(line);
@@ -108,38 +126,45 @@ int	main()
 	while (i < num_ants)
 		arr[i++] = strdup("start");
 
-	//free(line);
 	printf("num_ants = %d\n", num_ants);
 	while (getline(&line, &n, stdin) > 0)
 	{
 		if((p = strchr(line, '\n')))
 			*p = '\0';
-		//get start and end
-		if ()
+		if (line[0] == '#')
 		{
-
+			if (!strcmp(line, "##start"))
+			{
+				getline(&line, &n, stdin);
+				if((p = strchr(line, '\n')))
+					*p = '\0';
+				start_end = ft_strsplit(line, ' ');
+				start = strdup(start_end[0]);
+				free_str_arr(start_end);
+				continue ;
+			}
+			else if (!strcmp(line, "##end"))
+			{
+				getline(&line, &n, stdin);
+				if((p = strchr(line, '\n')))
+					*p = '\0';
+				start_end = ft_strsplit(line, ' ');
+				end = strdup(start_end[0]);
+				free_str_arr(start_end);
+				continue ;
+			}
 		}
 		else if (line[0] == 'L')
 		{
-			add_to_arr(line, arr, ants_arr, num_ants);
-			//if (!ants_in_same_room(line, arr))
-			//{
-			//	free(line);
-			//	printf("ERROR: multiple ants in the same room\n");
-			//	exit (0);
-			//}
+			add_to_arr(line, arr, ants_arr, num_ants, start, end);
 			line_count++;
 		}
-		/*
-		i = 0;
-		while(i < num_ants)
-		{
-			printf("%d %s\n", i + 1, arr[i]);
-			i++;
-		}
-		*/
 
 	}
+	free(line);
+	printf("line count: %d\n", line_count);
+	free(start);
+	free(end);
 	free_str_arr(arr);
 	free(ants_arr);
 	return (0);
@@ -148,3 +173,5 @@ int	main()
 //clang -Wall -Wextra -Werror ant_cop.c functions.c -o ant_cop
 
 //out put num of lines printed
+
+//./lem-in < maps/basic_maps/subject2-1.map | leaks -atExit -- ../ant_cop/./ant_cop
