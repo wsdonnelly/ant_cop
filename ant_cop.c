97abf8470ca1785f3d84e34dce85a6e4c5ad21e1
wsdonnelly <wsdonnelly@gmail.com>
2022-06-06 12:07:18 +0300

    ant_cop sketches

--- ant_cop.c
+++ ant_cop.c
@@ -6,7 +6,7 @@
 /*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 14:10:25 by wdonnell          #+#    #+#             */
-/*   Updated: 2022/06/05 16:18:23 by wdonnell         ###   ########.fr       */
+/*   Updated: 2022/06/06 12:06:52 by wdonnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,61 @@
 char	**ft_strsplit(char const *s, char c);
 void	free_str_arr(char **arr);
 
-void add_to_arr(char *line, char **arr)
+void add_to_arr(char *line, char **arr, int *ants_arr, int num_ants)
 {
 	char **ln;
 	char **info;
 	int i = 0;
+	int j;
 
 	ln = ft_strsplit(line, ' ');
 	while (ln[i])
 	{
+		j = 0;
+		while (j < num_ants)
+		{
+			ants_arr[j] = 0;
+			j++;
+		}
 		info = ft_strsplit(ln[i], '-');
+		if (ants_arr[j])
+		{
+			//same ant on same line
+			exit(0);
+		}
+		ants_arr[atoi(&info[0][1]) - 1] = 1;
 		//if (!valid_path(arr[atoi(&info[0][1]) - 1], info[1]))
 		//{
 		//		//free
 		//		printf("ERROR: invalid path\n");
 		//		exit (0);
 		//}
-		arr[atoi(&info[0][1]) - 1] = strdup(info[1]);
-		free_str_arr(info);
-		i++;
-		int j =0;
-		while(j < 5)
+		j = 0;
+		while (j < num_ants)
 		{
-			printf("%d %s\n", j + 1, arr[j]);
+			if (info[1][0] == arr[j][0])
+			{
+				if (!strcmp(info[1], arr[j]))
+				{
+					//same room on same line
+					exit(0);
+				}
+			}
 			j++;
 		}
+		arr[atoi(&info[0][1]) - 1] = strdup(info[1]);
+
+		free_str_arr(info);
+		i++;
+		
 	}
+	j =0;
+	while(j < num_ants)
+	{
+		printf("%d %s\n", j + 1, arr[j]);
+		j++;
+	}
+	printf("\n");
 	free_str_arr(ln);
 }
 /*
@@ -64,10 +93,13 @@ int	main()
 	int num_ants;
 	int line_count = 0;
 	char **arr;
+	int *ants_arr;
 	int i = 0;
+	char *p;
 
 	getline(&line, &n, stdin);
 	num_ants = atoi(line);
+	ants_arr = malloc(sizeof(int) * num_ants);
 	arr = malloc(sizeof(char *) * num_ants);
 	while (i < num_ants)
 		arr[i++] = strdup("start");
@@ -76,10 +108,12 @@ int	main()
 	printf("num_ants = %d\n", num_ants);
 	while (getline(&line, &n, stdin) > 0)
 	{
+		if((p = strchr(line, '\n')))
+			*p = '\0';
 		//printf("line: %s", line);
 		if (line[0] == 'L')
 		{
-			add_to_arr(line, arr);
+			add_to_arr(line, arr, ants_arr, num_ants);
 			//if (!ants_in_same_room(line, arr))
 			//{
 			//	free(line);
@@ -98,8 +132,11 @@ int	main()
 		*/
 
 	}
-
+	free_str_arr(arr);
+	free(ants_arr);
 	return (0);
 }
 
-//gcc -Wall -Wextra -Werror ant_cop.c -o ant_cop
+//clang -Wall -Wextra -Werror ant_cop.c functions.c -o ant_cop
+
+//out put num of lines printed
